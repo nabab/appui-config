8b4fdfae0d19792896b14a66618425e7c3204d9d
nabab <thomas.nabet@gmail.com>
2017-12-15 05:46:58 +0100

    Small graphic adaptations

--- src/mvc/html/panel/search.php
+++ src/mvc/html/panel/search.php
@@ -1,8 +1,8 @@
 <!-- HTML Document -->
 
-<div class="bbn-task-splitter" v-bbn-fill-height>
+<div class="bbn-task-splitter bbn-full-screen">
   <div class="bbn-task-search-container">
-    <div class="bbn-form-full bbn-c">
+    <div class="bbn-form-full bbn-c bbn-flex-width">
       <div class="bbn-block" style="margin: 0 2em">
         <select name="selection" class="bbn-xl" data-role="dropdownlist" data-bind="events: {change: change_selection}">
           <option value="user"><?=_("Mine")?></option>
@@ -10,8 +10,8 @@
           <option value="all"><?=_("All")?></option>
         </select>
       </div>
-      <div class="bbn-block" v-bbn-fill-width>
-        <input name="title" class="bbn-xl k-textbox" v-bbn-fill-width placeholder="<?=_("Search or Title for the new task")?>">
+      <div class="bbn-flex-fill">
+        <input name="title" class="bbn-xl k-textbox bbn-w-100" placeholder="<?=_("Search or Title for the new task")?>">
       </div>
       <div class="bbn-block" style="margin: 0 2em">
         <button class="k-button bbn-xl" data-bind="click: create_task">
